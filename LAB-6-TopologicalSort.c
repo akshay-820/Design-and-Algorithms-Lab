d601a63db5076fb65877d29144ff2b1d02be964a
akshay-820 <akshay200583@gmail.com>
2025-05-16 10:23:55 +0530

    LAB-6 Topological Sort

--- LAB-6-TopologicalSort.c
+++ LAB-6-TopologicalSort.c
@@ -26,6 +26,13 @@ int main() {
     printf("Enter number of vertices and edges: ");
     scanf("%d %d", &V, &E);
 
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            adj[i][j] = 0;
+        }
+    }
+
+    printf("Enter edges (u v): \n");
     for (int i = 0; i < E; i++) {
         scanf("%d %d", &u, &v);
         adj[u][v] = 1;
